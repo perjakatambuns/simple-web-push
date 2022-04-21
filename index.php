ccd37bde13163d06e14bf41cbf328bf09fe9e465
Rizky Maulana <rizeka1337@gmail.com>
2022-04-22 02:10:12 +0700

    simple web push and stats

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+	header('Content-Type: application/json; charset=utf-8');
+	if(file_exists('log_ssh_'.date("j.n.Y").'.log')){
+		$file = fopen('log_ssh_'.date("j.n.Y").'.log','r');
+		$nodes['status'] = true;
+		$nodes['data'] = array();
+		while (($line = stream_get_line($file, 1024 * 1024, "\n")) !== false) {
+			$data = explode('|',$line);
+			$nodes['data'][$data[1]]['total_attempts'] += $data[2];
+			if($data[3] == 'SUCCESS'){
+				$nodes['data'][$data[1]]['success_attempts'] += $data[2];
+			}else{
+				$nodes['data'][$data[1]]['success_attempts'] += 0;
+			}
+			if($data[3] == 'FAILED'){
+				$nodes['data'][$data[1]]['failed_attempts'] += $data[2];
+			}else{
+				$nodes['data'][$data[1]]['failed_attempts'] += 0;
+			}
+			
+			$nodes['data'][$data[1]]['detail_access'][] = array(
+													"status" => $data[3],
+													"attempts" => $data[2],
+													"user" => $data[4],
+													"ip" => $data[5],
+													"resolver" => base64_decode($data[6]),
+													"time" => base64_decode($data[7])
+												);
+		}
+		fclose($file);
+		echo json_encode($nodes);
+	}else{
+		$nodes['status'] = false;
+		$nodes['data'] = '';
+		echo json_encode($nodes);
+	}
+?>
---
+++ push.php
@@ -0,0 +1,17 @@
+<?php
+if(isset($_GET['id'],$_GET['node'],$_GET['status'],$_GET['user'],$_GET['count'],$_GET['ip'],$_GET['resolve'],$_GET['date1'],$_GET['date2'])){
+	$id = $_GET['id'];
+	$node = $_GET['node'];
+	$status = $_GET['status'];
+	$user = $_GET['user'];
+	$count = $_GET['count'];
+	$ip = $_GET['ip'];
+	$resolve = $_GET['resolve'];
+	$date1 = $_GET['date1'];
+	$date2 = $_GET['date2'];
+	$uniq = str_pad(rand(0,999), 5, "0", STR_PAD_LEFT);
+	
+	$data = $id.'|'.$node.'|'.$count.'|'.$status.'|'.$user.'|'.$ip.'|'.$resolve.'|'.$date1.'|'.$date2;
+	file_put_contents('log_ssh_'.date("j.n.Y").'.log', $data."\n", FILE_APPEND);
+}
+?>
